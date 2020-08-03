a537d864e8e75d80c5aa3574537ca43db2b25dfc
Kyle Stough <kstough00@gmail.com>
2020-08-03 01:17:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(array)
+def now_serving(x)
   puts "There is nobody waiting to be served!"
 end
