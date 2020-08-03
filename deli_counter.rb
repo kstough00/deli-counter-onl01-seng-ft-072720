6cd8ae695bc758e07e40b8eeacc09fd6c0fe13d9
Kyle Stough <kstough00@gmail.com>
2020-08-03 00:26:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,7 @@
 # Write your code here.
 
-katz_deli = []
+
 
 def line(array)
-  if array == 0
-    puts "The line is currently empty."
-  else
-    puts line
-  end
+  puts "The line is currently empty."
 end
