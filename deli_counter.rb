944f4abab0a1523449c95a2f4a457db4c359d6f6
Kyle Stough <kstough00@gmail.com>
2020-08-03 00:49:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 katz_deli = []
 
 def line(x)
-  line_array = []
+  new_array = []
   if x.length == 0
     puts "The line is currently empty."
   else
     x.with_each_index(1) do |name, index|
-      line_array << ("#{name}. #{index}")
+      new_array << ("#{name}. #{index}")
   end
   puts "The line is currently: #{"
 end
